e15cc92ddfb32a3945e6f8972a4b1043d2e0aba2
Oleg Pavlov <4pavlovon@gmail.com>
2016-03-05 01:06:25 +0300

    Fix migrate2

--- migrations/m160304_182516_tasks_projects.php
+++ migrations/m160304_182516_tasks_projects.php
@@ -8,6 +8,11 @@ class m160304_182516_tasks_projects extends Migration
     // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable("tasks", [
             "id" => $this->primaryKey(),
             "project_id" => $this->integer()->notNull(),
@@ -16,13 +21,13 @@ class m160304_182516_tasks_projects extends Migration
             "sort" => $this->integer(),
             "date" => $this->dateTime(),
             "link" => $this->string()
-        ]);
+        ], $tableOptions);
 
         $this->createTable("projects", [
             "id" => $this->primaryKey(),
             "project_name" => $this->string(255),
             "link" => $this->string()
-        ]);
+        ], $tableOptions);
 
         $this->createIndex("idx-projects-project_id", "tasks", "project_id");
         $this->addForeignKey("fk-projects-project_id", "tasks", "project_id", "projects", "id", "CASCADE");
