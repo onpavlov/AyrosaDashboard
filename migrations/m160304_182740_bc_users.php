1f6c3950833aca912a32ba379fdacd0255eccbb1
Oleg Pavlov <4pavlovon@gmail.com>
2016-03-05 01:03:44 +0300

    Fix migrate

--- migrations/m160304_182740_bc_users.php
+++ migrations/m160304_182740_bc_users.php
@@ -6,12 +6,17 @@ class m160304_182740_bc_users extends Migration
 {
     public function up()
     {
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable("bc_users", [
             "id" => $this->primaryKey(),
             "login" => $this->string()->notNull(),
             "firstname" => $this->string(255),
             "lastname" => $this->string(50)
-        ]);
+        ], $tableOptions);
     }
 
     public function down()
