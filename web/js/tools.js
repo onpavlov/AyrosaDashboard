9cc47235ef05d25a413760b0b2aed9d3902767ca
Oleg Pavlov <4pavlovon@gmail.com>
2016-03-20 23:45:28 +0300

    Fix update tools

--- web/js/tools.js
+++ web/js/tools.js
@@ -1,50 +1,56 @@
 $(document).ready(function() {
     $("button.parse_data").click(function() {
-        var data = [];
+        var len     = 0;
+        progress    = {};
+        progress.i  = 0;
+        progress.data = [];
+
         $("#result-bc").html("");
 
         $("input[type=checkbox]:checked").each(function(i, el) {
             var value = $(el).val();
 
+            /* Формируем объект с данными для обновления */
             if (value == "projects") {
-                parse({"action" : value + "Update"});
-                var projects = getProjects();
-
-                $(projects).each(function(i, el) {
-                    data.push({"action" : "taskUpdate", "params" : el});
-                });
+                updateProjects();
+                len = 1;
             } else {
-                data.push({"action" : value + "Update"});
+                progress.data.push({"action" : value + "Update"});
             }
         });
 
-        if (data.length) {
-            var step = 100 / data.length;
-            var progress = $(".progress-bar");
+        if (progress.data.length) {
+            progress.step   = 100 / (progress.data.length + len);
+            progress.bar    = $(".progress-bar");
             $(".progress").removeAttr("style");
-            progress.addClass("active");
+            progress.bar.addClass("active");
 
-            $(data).each(function(i, el) {
+            $(progress.data).each(function(i, el) {
                 parse(el);
-                var current = step * (i + 1);
-                progress.html(~~current + "%");
-                progress.data("aria-valuenow", ~~current);
-                progress.width(~~current + "%");
             });
-
-            progress.removeClass("active");
         }
     });
 
-    function parse(item) {
+    function parse(item, async) {
         $.ajax({
             url: "tools/update",
             dataType: "json",
             data: item,
-            async: false,
+            async: async || true,
             success: function(data) {
-                var result = "";
+                /* Обновление прогресс-бара */
+                progress.current = progress.step * (progress.i + 1);
+                progress.bar.html(~~progress.current + "%");
+                progress.bar.data("aria-valuenow", ~~progress.current);
+                progress.bar.width(~~progress.current + "%");
+                progress.i += 1;
 
+                if (~~progress.current == 100) {
+                    progress.bar.removeClass("active");
+                }
+
+                /* Вывод сообщений в лог результатов */
+                var result = "";
                 $(data).each(function(i, el) {
                     if (el.status == "success") {
                         result += "<p class='text-success'>" + el.message + "</p>"
@@ -60,18 +66,19 @@ $(document).ready(function() {
         });
     }
 
-    function getProjects() {
-        var result;
+    /* Запрашивает список  */
+    function updateProjects() {
+        parse({"action" : "projectsUpdate"}, false);
 
         $.ajax({
             url: "tools/projects",
             async: false,
             dataType: "json",
             success: function(data) {
-                result = data;
+                $(data).each(function(i, el) {
+                    progress.data.push({"action" : "taskUpdate", "params" : el});
+                });
             }
         });
-
-        return result;
     }
 });
